78c785cc8151b709caa6750a3f0f7f277d8c1bcc
Snorone <tobias.hurtig@gritacademy.se>
2024-01-08 15:35:32 +0100

    Update fetch.js

--- modules/fetch.js
+++ modules/fetch.js
@@ -2,6 +2,7 @@ const BAERER_KEY =
   "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmEzMGUzMDUxNDk2MzYzMGIzZjBmMTFlNDVjZmZlZiIsInN1YiI6IjY1ODAwNTQ0MGU2NGFmMDgzOWE4NTY2YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.mXFG31A3Gc9TIa9a-tDRXc2PwfYME01u_gmyEt5ipL4";
 
 export async function fetchInfo(userSearch) {
+  console.log(userSearch);
   url = `https://api.themoviedb.org/3/search/multi?query=${userSearch}&include_adult=false&language=en-US&page=1`;
 
   const options = {
