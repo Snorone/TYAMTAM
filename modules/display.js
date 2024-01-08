a291494af5331a3a1ebae5f5c6c0b1aa57822af4
Ton <146097352+wiangin@users.noreply.github.com>
2024-01-08 17:39:56 +0100

    change

--- modules/display.js
+++ modules/display.js
@@ -1,11 +1,18 @@
-const imageBaseUrl = `https://image.tmdb.org/t/p/w400/`;
+const imageBaseUrl = `https://image.tmdb.org/t/p/w200/`;
 function displayMovie(movie){
-    //console.log(movie);
+    console.log(movie);
     for(const movieList of movie.results){
-        //console.log(movieList);
         const moviePoster = movieList.poster_path;
-        console.log(moviePoster);
         const movieInfoDiv = document.createElement('div');
+        createAndAppendElement('img', imageBaseUrl+moviePoster,movieInfoDiv);
+        
+        if(movieList.media_type === 'movie'){
+            createAndAppendElement('h3',movieList.title,movieInfoDiv);
+        }
+        else{
+            createAndAppendElement('h3',movieList.name,movieInfoDiv);
+        }
+
         document.body.append(movieInfoDiv);
     }
 }
