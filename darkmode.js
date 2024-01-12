036ebe174ec6269f1a5a28d96f008bb92ce1b2ea
Melker Dahl <jaghetermelker@gmail.com>
2024-01-12 19:44:17 +0100

    darkmode

--- darkmode.js
+++ darkmode.js
@@ -1,66 +1,83 @@
 let darkModeBody = document.body;
-// function changeSvgs() {
-// 	const contactUs = document.querySelector(".callUs");
-// 	const chatNow = document.querySelector(".chatNow");
-// 	const getStarted = document.querySelector(".getStarted");
-// 	contactUs.src = "./assets/call-dark.svg";
-// 	chatNow.src = "./assets/chatt-dark.svg";
-// 	getStarted.src = "./assets/mail-dark.svg";
-// }
+const getSvgs = document.querySelector(".svgs");
+
+function dmSvgFunc() {
+  const contactUs = document.querySelector(".callUs");
+  const chatNow = document.querySelector(".chatNow");
+  const getStarted = document.querySelector(".getStarted");
+  contactUs.src = "./assets/call-dark.svg";
+  chatNow.src = "./assets/chatt-dark.svg";
+  getStarted.src = "./assets/mail-dark.svg";
+}
+function lmSvgFunc() {
+  const contactUs = document.querySelector(".callUs");
+  const chatNow = document.querySelector(".chatNow");
+  const getStarted = document.querySelector(".getStarted");
+  contactUs.src = "./assets/Group 23.svg";
+  chatNow.src = "./assets/Group 24.svg";
+  getStarted.src = "./assets/Group 20.svg";
+}
 const darkModeBtn = document.querySelector("#switch");
 
-// 1
 const prefersDarkTheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-if (prefersDarkTheme.matches) {
-	darkModeBody.classList.add("darkmode");
-	darkModeBtn.setAttribute("checked", "true");
-} else {
-	darkModeBody.classList.remove("darkmode");
-}
-const currentTheme = localStorage.getItem("theme");
-console.log(currentTheme);
+// const currentTheme = localStorage.getItem("theme");
 let theme = "light";
-
-if (currentTheme == "dark") {
-	darkModeBody.classList.add("darkmode");
-	console.log(currentTheme);
+if (prefersDarkTheme.matches) {
+  darkModeBody.classList.add("darkmode");
+  darkModeBtn.setAttribute("checked", "true");
+  theme = "dark";
+  if (darkModeBody.contains(getSvgs)) {
+    dmSvgFunc();
+  }
+} else {
+  darkModeBody.classList.remove("darkmode");
+  darkModeBtn.removeAttribute("checked");
+  theme = "light";
+  if (darkModeBody.contains(getSvgs)) {
+    lmSvgFunc();
+  }
 }
 if (darkModeBody.classList.contains("darkmode")) {
-	theme = "dark";
+  theme = "dark";
+}
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+  darkModeBody.classList.add("darkmode");
+  darkModeBtn.setAttribute("checked", "true");
+  if (darkModeBody.contains(getSvgs)) {
+    dmSvgFunc();
+  }
+} else {
+  darkModeBody.classList.remove("darkmode");
+  darkModeBtn.removeAttribute("checked");
+
+  theme = "light";
+  if (darkModeBody.contains(getSvgs)) {
+    lmSvgFunc();
+  }
+}
+if (darkModeBody.classList.contains("darkmode")) {
+  theme = "dark";
 }
 
 darkModeBtn.addEventListener("click", function () {
-	darkModeBody.classList.toggle("darkmode");
-
-	if (darkModeBody.classList.contains("darkmode")) {
-		theme = "dark";
-	}
-	localStorage.setItem("theme", theme);
-	// changeSvgs();
-	console.log(theme);
-	// preventDefault();
+  localStorage.clear();
+  if (darkModeBody.classList.contains("darkmode")) {
+    darkModeBody.classList.remove("darkmode");
+    theme = "light";
+    if (darkModeBody.contains(getSvgs)) {
+      lmSvgFunc();
+    }
+  } else {
+    localStorage.clear();
+    darkModeBody.classList.toggle("darkmode");
+    theme = "dark";
+    if (darkModeBody.contains(getSvgs)) {
+      dmSvgFunc();
+    }
+  }
+  localStorage.setItem("theme", theme);
 });
 
-// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-
-// const currentTheme = localStorage.getItem("theme");
-
-// if (currentTheme == "dark") {
-// 	darkModeBody.classList.toggle("darkmode");
-// } else if (currentTheme == "light") {
-// 	darkModeBody.classList.toggle("");
-// }
-
-// darkModeBtn.addEventListener("click", function () {
-// 	if (prefersDarkScheme.matches) {
-// 		darkModeBody.classList.toggle("light-mode");
-
-// 		let theme = darkModeBody.classList.contains("light-mode");
-// 	} else {
-// 		darkModeBody.classList.toggle("darkmode");
-// 		let theme = darkModeBody.classList.contains("darkmode") ? "dark" : "light";
-// 	}
-
-// 	localStorage.setItem("theme", theme);
-// });
+// !
